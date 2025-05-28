7b45d3e07e4dce5515b05b497515bb443da3a211
Anmol Pandey <axp8948@mavs.uta.edu>
2025-05-28 12:50:22 -0500

    Post-Form and RTE

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Header from "./Header/Header";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
